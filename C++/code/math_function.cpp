d8923a1e65db104df9afcc25279381256beda440
RogerMhetre <rogermhetre18@gmail.com>
2025-12-28 19:46:42 +0530

    Add math_function.cpp

---
+++ C++/code/math_function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    double x = 3.14;
+    double y = 4;
+    double z;
+
+    //z = std::max(x, y);
+    //z = std::min(x, y);
+    //z = pow(2, 4);
+    //z = sqrt(9);
+    //z = abs(-19);
+    //z = round(x);
+    //z = ceil(x); round up
+    //z = floor(x); round down
+
+    //head to www.cplusplus.com/reference/cmath/ 
+
+    std::cout << z;
+
+    return 0;
+}
