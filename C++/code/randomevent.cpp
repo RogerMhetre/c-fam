24824518ca7417a9e2e4059f94ee48191e44b4aa
RogerMhetre <rogermhetre18@gmail.com>
2025-12-28 19:47:25 +0530

    Add randomevent.cpp

---
+++ C++/code/randomevent.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ctime>
+
+int main(){
+
+    srand(time(0));
+    int randNum = (rand() % 5) + 1;
+
+    switch(randNum){
+
+        case 1: std::cout << "You win a bumper sticker"; break;
+        case 2: std::cout << "You win a T-shirt"; break; 
+        case 3: std::cout << "You win a free lunch"; break;
+        case 4: std::cout << "You win a Google pay gift card"; break;
+        case 5: std::cout << "You win a Concert tickets"; break;
+    }
+
+    return 0;
+}
