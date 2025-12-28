81f75461f718970e3cf851099b6626d5ae315873
RogerMhetre <rogermhetre18@gmail.com>
2025-12-28 19:45:36 +0530

    Add const.cpp

---
+++ C++/code/const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    //const stands for constant and the value of the variable we assign with const can't be changed
+    //If it is changed by someone later in the program it will just cause an error
+    //EXAMPLE:
+
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+    return 0;
+}
