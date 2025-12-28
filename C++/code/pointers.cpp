1ac29005cd2768b0e3316e64bee1e836b985bb26
RogerMhetre <rogermhetre18@gmail.com>
2025-12-28 19:47:19 +0530

    Add pointers.cpp

---
+++ C++/code/pointers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+int main(){
+
+    // pointers ==> variable that stores a memory address of another variable 
+    //              sometimes it's easier to work with an address
+    
+    // & address-of operator
+    // * dereference operator 
+    
+    std::string name = "Juice";
+    int age = 21;
+    std::string freePizzas[5] = {"pizza1", "pizza2", "pizza3", "pizza4", "pizza5" }
+
+    std::string *pName = &name;
+    int *pAge = &age;
+    std::string *pfreePizzas = &freePizzas;
+
+    std::cout << *pName << std::endl;
+    std::cout << *pAge;
+
+    return 0;
+}
